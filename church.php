782740b149c94b0fa569b67f7297e90154fe5ad8
marcEssam48 <42551703+marcEssam48@users.noreply.github.com>
2018-10-05 20:40:30 +0300

    Add files via upload

---
+++ church.php
@@ -0,0 +1,19 @@
+
+<div class="container-fluid mt-5" >
+
+            <!-- Heading -->
+            <div class="card mb-4 wow fadeIn">
+
+                <!--Card content-->
+                <div class="card-body d-sm-flex justify-content-between">
+
+                    <h4 class="mb-2 mb-sm-0 pt-1">
+                        <h3  style="text-align: center;color: #2a1758;margin-right: 240px;">كنيسة رئيس الملائكة الجليل ميخائيل بالظاهر</h3>
+
+                    </h4>
+
+
+
+                </div>
+
+            </div>
